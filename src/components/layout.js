cbda2d6722772494d294eeadeed37db6c50afcfc
Matthew Shelley <46871802+mattscripted@users.noreply.github.com>
2020-07-10 12:40:42 -0400

    Clean up blog from defaults: fix linting errors with standardjs, add gh-pages deployment script, update config, and remove license

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,7 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react'
+import { Link } from 'gatsby'
 
-import { rhythm, scale } from "../utils/typography"
+import { rhythm, scale } from '../utils/typography'
 
 const Layout = ({ location, title, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`
@@ -13,15 +13,15 @@ const Layout = ({ location, title, children }) => {
         style={{
           ...scale(1.5),
           marginBottom: rhythm(1.5),
-          marginTop: 0,
+          marginTop: 0
         }}
       >
         <Link
           style={{
-            boxShadow: `none`,
-            color: `inherit`,
+            boxShadow: 'none',
+            color: 'inherit'
           }}
-          to={`/`}
+          to='/'
         >
           {title}
         </Link>
@@ -31,16 +31,16 @@ const Layout = ({ location, title, children }) => {
     header = (
       <h3
         style={{
-          fontFamily: `Montserrat, sans-serif`,
-          marginTop: 0,
+          fontFamily: 'Montserrat, sans-serif',
+          marginTop: 0
         }}
       >
         <Link
           style={{
-            boxShadow: `none`,
-            color: `inherit`,
+            boxShadow: 'none',
+            color: 'inherit'
           }}
-          to={`/`}
+          to='/'
         >
           {title}
         </Link>
@@ -50,18 +50,18 @@ const Layout = ({ location, title, children }) => {
   return (
     <div
       style={{
-        marginLeft: `auto`,
-        marginRight: `auto`,
+        marginLeft: 'auto',
+        marginRight: 'auto',
         maxWidth: rhythm(24),
-        padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
+        padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`
       }}
     >
       <header>{header}</header>
       <main>{children}</main>
       <footer>
         © {new Date().getFullYear()}, Built with
-        {` `}
-        <a href="https://www.gatsbyjs.org">Gatsby</a>
+        {' '}
+        <a href='https://www.gatsbyjs.org'>Gatsby</a>
       </footer>
     </div>
   )
