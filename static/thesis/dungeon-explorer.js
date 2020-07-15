7697ca42b6e8f3eb863ec052f6c881f7dcba3a31
mattscripted <46871802+mattscripted@users.noreply.github.com>
2020-07-15 15:30:54 -0400

    Add "Dungeon Explorer" link to thesis page (#14)

---
+++ static/thesis/dungeon-explorer.js
@@ -0,0 +1,10 @@
+// Copyright (c) 2013, Matthew Shelley. All rights reserved.
+
+/*!	global
+	Initialize DungeonExplorer so it can take over
+*/
+$(document).ready(function()
+{
+	// Launch the game, passing in the game data file
+	DungeonExplorer.init('dungeon-explorer/game-data/game-data.xml');
+});
